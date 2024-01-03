6cfb4af2da90ebe038fb5f2b5ccacfe523cce40f
XDarren11 <jhosiescobar@gmail.com>
2024-01-03 00:49:53 -0600

    Subida de proyecto

---
+++ Interpreter.java
@@ -0,0 +1,86 @@
+package interpreter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Interpreter {
+
+    static boolean existenErrores = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("El uso correcto: interprete [archivo.txt]\n");
+
+            for (String a : args) {
+                System.out.println("\n\n\n//////////////////////////\n prueba:" + a + "\n");
+                ejecutarArchivo(a);
+
+            }
+            System.exit(64);
+        } else if (args.length == 1) {
+            ejecutarArchivo(args[0]);
+
+        } else {
+            System.out.println("prompt");
+            ejecutarPrompt();
+        }
+    }
+
+    private static void ejecutarArchivo(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        ejecutar(new String(bytes, Charset.defaultCharset()));
+
+        // Se indica que existe un error
+        if (existenErrores)
+            System.exit(65);
+    }
+
+    private static void ejecutarPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print(">>> ");
+            String linea = reader.readLine();
+            if (linea == null)
+                break; // Presionar Ctrl + D
+            ejecutar(linea);
+            existenErrores = false;
+        }
+    }
+
+    private static void ejecutar(String source) {
+
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scan();
+        int i = 0;
+
+        for (Token token : tokens) {
+            if (token.tipo == TipoToken.ERROR_LEXICAL) {
+                i++;
+                reportar(token.linea, token.columnaE, token.lexema);
+
+            }
+        }
+
+        if (i != 0) {
+            System.exit(0);
+        }
+
+        Parser parser = new ASDR(tokens);
+        parser.parse();
+
+    }
+
+    private static void reportar(int linea, Integer posicion, Object mensaje) {
+        System.err.println(
+                "[linea " + linea + " posicion " + posicion + "] : " + mensaje);
+        existenErrores = true;
+    }
+
+}
---
+++ TipoToken.java
@@ -0,0 +1,21 @@
+package interpreter;
+
+public enum TipoToken {
+    // Tokens de un sólo caracter
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // Tokens de uno o dos caracteres
+    BANG, EQUAL, GREATER, LESS,
+    BANG_EQUAL, EQUAL_EQUAL,
+    GREATER_EQUAL, LESS_EQUAL,
+
+    // Literales
+    IDENTIFIER, STRING, NUMBER, COMMENT,
+
+    // Palabras clave
+    AND, ELSE, FALSE, FUN, FOR, IF, NULL, OR,
+    PRINT, RETURN, TRUE, VAR, WHILE, ERROR_LEXICAL,
+
+    error, EOF
+}
